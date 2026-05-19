52737381c92ca740b5afbb60c37ac40d176ff781
ahMAD ASSadi <ahmadasadihengami@gmail.com>
2026-05-19 23:30:13 +0330

    refactor: remove unused model imports from __init__.py

--- src/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from models.qa import (  # noqa: F401
-    CommunityAnswer,
-    CommunityAnswerAttachment,
-    CommunityQuestion,
-    CommunityQuestionAttachment,
-)
-from models.users import User, UserRole  # noqa: F401
